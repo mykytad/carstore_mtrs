91247412c8666d549eeb1afde6842fe80cafc5dc
Mykyta Dynnik <mykytadynnik@gmail.com>
2024-07-24 17:06:57 +0300

    fix rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,5 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, 
-    :registerable, 
-    :recoverable,
-    :rememberable,
-    :validatable,
-    :api
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 end
